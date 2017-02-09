cb60ce1b22fb811e7e49986958a200f4c56806f2
bradleyal53 <bradleyal@cwu.edu>
2017-02-09 13:43:34 -0800

    Profile Pages
    Uploading profile page.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { ProfilePage } from './profile/profile';
+//import { SignInPage } from '../../../pages/profile/sign-in/signin';
+//import { SignUpPage } from '../../../pages/profile/sign-up/signup';
 
 @Component({
-  selector: 'page-profile',
+  selector: 'profile',
   templateUrl: 'profile.html'
 })
+
 export class ProfilePage {
-
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController) {}
+  signIn(){
+    //this.navCtrl.push(SignInPage);
   }
-
+  signUp(){
+    //this.navCtrl.push(SignUpPage);
+  }
 }
