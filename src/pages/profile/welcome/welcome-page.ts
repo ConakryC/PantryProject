998f6ef4cdd635c3847f05ee02efb618849865c1
Dakota Pierone <dakotapierone@yahoo.com>
2017-03-09 12:21:24 -0800

    Food jokes on welcome page

--- src/pages/profile/welcome/welcome-page.ts
+++ src/pages/profile/welcome/welcome-page.ts
@@ -1,25 +1,53 @@
 import { Component } from '@angular/core';
-
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { NavController } from 'ionic-angular';
 import { Auth, User } from '@ionic/cloud-angular';
 import { ProfilePage } from '../../profile/profile';
 
-@Component ({
+@Component({
   selector: 'page-welcome',
   templateUrl: 'welcome-page.html'
 })
 
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public user:User, public auth: Auth) {
-  console.log(user);
+  private headers: Headers;
+  private opt: RequestOptions;
+  private foodJoke: string;
+  private foodTrivia: string;
 
- }
+  constructor(public navCtrl: NavController, public user: User, public auth: Auth, public http: Http) {
+    console.log(user);
 
- logout() {
-   this.auth.logout();
-   this.navCtrl.setRoot(ProfilePage);
- }
+    this.foodJoke = "";
+    this.foodTrivia = "";
 
+    //Setup the headers we use for the API calls
+    this.headers = new Headers();
+    this.headers.append('X-Mashape-Key', 'FBiqUe796amshHCRsuDjukypRhO4p1C7p0FjsnURXVaA5HhxLS');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('Accept', 'application/json ');
 
+    //Add header options
+    this.opt = new RequestOptions({
+      headers: this.headers
+    });
+  }
+
+  logout() {
+    this.auth.logout();
+    this.navCtrl.setRoot(ProfilePage);
+  }
+
+  getJoke() {
+    this.http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random", this.opt).map(res => res.json()).subscribe(js => {
+      this.foodJoke = js.text;
+    });
+  }
+
+  getTrivia() {
+    this.http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/trivia/random", this.opt).map(res => res.json()).subscribe(js => {
+      this.foodTrivia = js.text;
+    });
+  }
 }
