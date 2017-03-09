2c725cabe3d21b3aa6e18ac15df06db81276808e
Conakry Coggins <ConakryC@gmail.com>
2017-03-09 11:32:39 -0800

    Revert "Revert "Update app.module.ts""
    This reverts commit 2ee27fd858511c9ae2cf1afd4156b7744fecafe9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { MyApp } from './app.component';
 import { ProfilePage } from '../pages/profile/profile';
+import { WelcomePage } from '../pages/profile/welcome/welcome-page';
 import { RecipesPage } from '../pages/recipes/recipes';
 import { ItemPage } from '../pages/products/item/item-page';
 import { ProductsPage } from '../pages/products/products';
@@ -12,12 +15,19 @@ import { PantryListService } from '../providers/pantry-list';
 import { ProductHelper } from '../providers/product-helper';
 import { RecipeDetails } from '../pages/recipes/recipe-details/recipe-details'
 import { RecipeListService } from '../providers/recipe-list';
+import { Platform } from 'ionic-angular';
 
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'b9279193'
+  }
+};
 
 @NgModule({
     declarations: [
         MyApp,
         ProfilePage,
+        WelcomePage,
         RecipesPage,
         ItemPage,
         ProductsPage,
@@ -30,12 +40,14 @@ import { RecipeListService } from '../providers/recipe-list';
         TabsPage
     ],
     imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        CloudModule.forRoot(cloudSettings)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
         MyApp,
         ProfilePage,
+        WelcomePage,
         RecipesPage,
         ItemPage,
         ProductsPage,
@@ -49,4 +61,18 @@ import { RecipeListService } from '../providers/recipe-list';
     ],
     providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, PantryListService, RecipeListService, ProductHelper]
 })
-export class AppModule { }
+export class AppModule {
+  rootPage;
+
+  constructor(platform: Platform, public auth: Auth) {
+    platform.ready().then(() => {
+
+      if(this.auth.isAuthenticated()) {
+        this.rootPage = WelcomePage;
+      } else {
+        this.rootPage = ProfilePage;
+      }
+
+    });
+  }
+}
